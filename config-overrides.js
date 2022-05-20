7288a461936e97927eac2a0404ece453a061114d
Arkadiusz Martenka <arkadiusz.martenka@gmail.com>
2022-05-20 14:11:40 +0200

    git init

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+// npm i -D customize-cra react-app-rewire-alias
+// dodawanie sciezek z zewnatrz do naszej aplikacji - wczesniej trzeba dodac do package.json
+
+// stworzyc tsconfig.paths.json {
+//   "compilerOptions": {
+//     "baseUrl": ".",
+//     "paths": {
+//       "types": [
+//         "../mega-ads-back/types"
+//       ]
+//     }
+//   }
+// }
+
+
+const {override} = require('customize-cra');
+const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous');
+
+module.exports = {
+    webpack: override(
+        aliasDangerous(configPaths('./tsconfig.paths.json'))
+    ),
+};
